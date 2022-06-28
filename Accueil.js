6e552e32f3a8aefd9cd09001f23a990c7ddf9651
BeFreecs <timothy.muggeo@gmail.com>
2022-06-28 16:54:10 +0200

    Accueil.js

--- Accueil.js
+++ Accueil.js
@@ -1,15 +1,14 @@
-let btnAccount = document.querySelector("input[name=newCompte]")
-btnAccount.addEventListener ("click", function(){
-    location.href = "http://127.0.0.1:5500/newAccount.html";
+let btnAccount = document.querySelector("input[name=newCompte]");
+btnAccount.addEventListener("click", function () {
+  location.href = "http://127.0.0.1:5500/newAccount.html";
 });
 
-
-let btnRetrait = document.querySelector("input[name=retrait]")
-btnRetrait.addEventListener ("click", function(){
-    location.href = "http://127.0.0.1:5501/Retrait.html";
+let btnRetrait = document.querySelector("input[name=retrait]");
+btnRetrait.addEventListener("click", function () {
+  location.href = "http://127.0.0.1:5501/Retrait.html";
 });
 
-let btnAgios = document.querySelector("input[name=agios]")
-btnAgios.addEventListener ("click", function(){
-    location.href = "http://127.0.0.1:5500/Agios.html";
+let btnAgios = document.querySelector("input[name=agios]");
+btnAgios.addEventListener("click", function () {
+  location.href = "http://127.0.0.1:5500/Agios.html";
 });
